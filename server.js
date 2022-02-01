63756665168787834561ca18d98ec5ab75ad3cb3
Tommy Wong <>
2022-02-01 14:37:00 +0800

    redirect to https

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// function requireHTTPS(req, res, next) {
-//   console.log('requireHTTPS');
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure) {
-//     console.log('redirect: ' + 'https://' + req.headers.host + req.url);
-//     return res.redirect('https://' + req.headers.host + req.url);
-//   }
-//   console.log('next');
-//   next();
-// }
+function requireHTTPS(req, res, next) {
+  console.log('requireHTTPS');
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure) {
+    console.log('redirect: ' + 'https://' + req.headers.host + req.url);
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  console.log('next');
+  next();
+}
 
 const express = require('express');
 const path = require('path');
@@ -15,7 +15,7 @@ const path = require('path');
 const app = express();
 const port = 4200;
 
-// app.use(requireHTTPS);
+app.use(requireHTTPS);
 app.use(express.static(__dirname + '/dist/ustdboat-forum'));
 
 app.get('/*', function (req, res) {
