02f582de73624ad8f13c6d4abbed5eb81fd4dfce
Tommy Wong <>
2022-02-01 14:53:41 +0800

    move env to heroku dashboard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://pyret1uvgk.execute-api.ap-southeast-1.amazonaws.com/dev',
-  xApiKey: 'tros2K5xguC2IhRlNALROlJsroBJKr951gIYxSa0',
-  auth0Domain: 'dev-feuvbc3k.us.auth0.com',
-  auth0ClientId: 'loBVYiglzMWAZE7MHBl24WOi4MHBl7Ac'
+  production: true
 };
