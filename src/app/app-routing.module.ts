9b7736ca7f1a7acb4e09a2d654ae29a0d72c2e1f
Tommy Wong <>
2022-02-08 11:31:52 +0800

    init file upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from "./layout/main-page/main-page.component";
+import {TestMainComponent} from "./test-main/test-main.component";
+import {AuthGuard} from "@auth0/auth0-angular";
+import {DashboardComponent} from "./layout/dashboard/dashboard.component";
 
 const routes: Routes = [
   {path: 'mainpage', component: MainPageComponent},
-  // {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-  // {path: 'test', component: TestMainComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'test', component: TestMainComponent},
   {path: '**', redirectTo: 'mainpage', pathMatch: 'full'}
 ];
 
--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import jwt_decode from "jwt-decode";
+import {Auth0Service} from "../../services/auth0.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+  constructor(private auth0: Auth0Service) {
+    console.log(`[${this.constructor.name}] constructor`);
   }
 
   ngOnInit(): void {
+    console.log(`[${this.constructor.name}] ngOnInit`);
+    console.log(`access token: ${this.auth0.accessToken}`);
+    console.log(`decoded token: ${JSON.stringify(jwt_decode(this.auth0.accessToken))}`);
+    const decodedToken: any = jwt_decode(this.auth0.accessToken);
+    console.log(`isAdmin: ${decodedToken['http://demozero.net/roles'].includes('Admin2')}`);
   }
 
+  uploadStudentCsv(event: any) {
+    console.log(event);
+    let csv: FileList = event.target.files
+    if (csv && csv.length > 0 && csv.item(0)) {
+      let file: File | any = csv.item(0);
+      console.log(file.name);
+      console.log(file.size);
+      console.log(file.type);
+      let reader: FileReader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e) => {
+        let csv: string = reader.result as string;
+        console.log(csv);
+      }
+    }
+  }
 }
