5d35c9b04fdd44d17ee68080ed4b07b6e7bae283
Tommy Wong <>
2022-02-11 13:29:59 +0800

    add pro env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   forumApiUrl: 'https://pyret1uvgk.execute-api.ap-southeast-1.amazonaws.com/dev',
+  auth0ManagementApiUrl: 'https://bh0txw4pzb.execute-api.ap-southeast-1.amazonaws.com/dev',
   xApiKey: 'tros2K5xguC2IhRlNALROlJsroBJKr951gIYxSa0',
   auth0Domain: 'dev-feuvbc3k.us.auth0.com',
   auth0ClientId: 'loBVYiglzMWAZE7MHBl24WOi4MHBl7Ac'
