5a44aab25e1f390e278846d56ee4d5b8dd0f2c2e
Tommy Wong <>
2022-02-11 13:58:09 +0800

    disable production log

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  if (window) {
+    window.console.log = window.console.warn = window.console.info = function () {
+      // Don't log anything.
+    };
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
