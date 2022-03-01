f9b3b641e38e4cfdbc7d11ad0e35f250ecb26c70
Tommy Wong <>
2022-03-01 23:07:16 +0800

    add Training Statistics table and csv download

--- src/app/layout/dashboard-page/dashboard-page.component.ts
+++ src/app/layout/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ForumDashboardBackendService} from "../../services/aws-lambda/forum-dashboard-backend.service";
+import {faDownload} from "@fortawesome/free-solid-svg-icons";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,25 +9,54 @@ import {ForumDashboardBackendService} from "../../services/aws-lambda/forum-dash
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
+  faDownload = faDownload;
 
   trainingStatisticList: any;
-  totalTraining: any;
+  totalTraining: number;
+  isLoading: boolean;
 
   constructor(private restful: ForumDashboardBackendService) {
     console.log(`[${this.constructor.name}] constructor`);
+    this.isLoading = true;
+    this.totalTraining = 0;
   }
 
   ngOnInit(): void {
     console.log(`[${this.constructor.name}] ngOnInit`);
     this.restful.getTrainingStatistic().subscribe({
-      next: value => {
-        this.trainingStatisticList = value.trainingStatistics;
-        this.totalTraining = value.totalTraining;
-      }
+      next: result => {
+        this.trainingStatisticList = result.trainingStatistics;
+        this.totalTraining = result.totalTraining;
+      },
+      complete: () => this.isLoading = false
     });
   }
 
   public calculatePercentage(input: number) {
     return (input / this.totalTraining * 100).toFixed(0);
+  }
+
+  public downloadStatistics() {
+    this.restful.getTrainingStatistic().subscribe({
+      next: (result) => {
+        this.exportDataToCsv(result.trainingStatistics);
+      }
+    });
+  }
+
+  private exportDataToCsv(data: any) {
+    console.log(`data: `, data)
+    const replacer = (key: string, value: string) => value === null ? '' : value // specify how you want to handle null values here
+    const header = Object.keys((data)[0]);
+
+    const csv = [
+      header.join(','), // header row first
+      // @ts-ignore
+      ...data.map((row) => header.map((fieldName) => JSON.stringify(row[fieldName], replacer)).join(','))
+    ].join('\r\n');
+
+    const blob = new Blob([csv], {type: 'text/csv'});
+    console.log(`blob: `, typeof (blob));
+    saveAs(blob, "TrainingStatistic.csv");
   }
 }
