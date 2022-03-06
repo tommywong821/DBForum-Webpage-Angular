05fda865cef582a73a0ffc0aeacbc6e77cca7dbc
Tommy Wong <>
2022-03-06 23:09:19 +0800

    init updateTrainingDate ngrx

--- src/app/ngrx/training-data/training-data.effect.ts
+++ src/app/ngrx/training-data/training-data.effect.ts
@@ -2,10 +2,11 @@ import {Injectable} from "@angular/core";
 import {Actions, createEffect, ofType} from "@ngrx/effects";
 import {Store} from "@ngrx/store";
 import {selectIsLoaded} from "./training-data.selector";
-import {combineLatest, filter, of, switchMap, withLatestFrom} from "rxjs";
+import {combineLatest, filter, map, of, switchMap, withLatestFrom} from "rxjs";
 import {ForumMainPageBackendService} from "../../services/aws-lambda/forum-main-page-backend.service";
 
 import * as trainingDataAction from './training-data.action';
+import {ITraining} from "../../model/forum/ITraining";
 
 @Injectable()
 export class TrainingDataEffect {
@@ -24,6 +25,21 @@ export class TrainingDataEffect {
       ),
       switchMap(([trainingList]) => {
         const isLoaded: boolean = true;
+        return of(trainingDataAction.getTrainingDataListComplete({isLoaded, trainingList}));
+      })
+    )
+  )
+
+  updateTrainingDataList$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(trainingDataAction.updateTrainingDataList),
+      map(action => ({
+        type: action.type,
+        payload: action.trainingList
+      })),
+      switchMap((action) => {
+        const isLoaded: boolean = true;
+        const trainingList: Array<ITraining> = action.payload;
         return of(trainingDataAction.getTrainingDataListComplete({isLoaded, trainingList}));
       })
     )
