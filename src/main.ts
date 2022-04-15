3402c3a378637704847e26b85b40886aef510315
Tommy Wong <>
2022-04-15 19:13:17 +0800

    Revert "enable prd log"
    This reverts commit 3a5bcdb3f5dcf42d485db242724fcbbf0195667a.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  //
-  // if (window) {
-  //   window.console.log = window.console.warn = window.console.info = function () {
-  //     // Don't log anything.
-  //   };
-  // }
+
+  if (window) {
+    window.console.log = window.console.warn = window.console.info = function () {
+      // Don't log anything.
+    };
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
