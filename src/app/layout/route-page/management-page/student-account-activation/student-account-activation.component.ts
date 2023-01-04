63560a6846f5fdf4f9db4f409b80615382a6b676
Tommy Wong <tommywong821@gmail.com>
2023-01-04 21:09:52 +0800

    migrate get student account status to mongodb

--- src/app/layout/route-page/management-page/student-account-activation/student-account-activation.component.ts
+++ src/app/layout/route-page/management-page/student-account-activation/student-account-activation.component.ts
@@ -1,26 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {IDropdownSettings} from "ng-multiselect-dropdown";
-import {ForumBackendManagementService} from "../../../../services/aws-lambda/forum-backend-management.service";
-import {IStudent} from "../../../../model/forum/IStudent";
-import {UntypedFormBuilder, UntypedFormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { ForumBackendManagementService } from '../../../../services/aws-lambda/forum-backend-management.service';
+import { IStudent } from '../../../../model/forum/IStudent';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-account-activation',
   templateUrl: './student-account-activation.component.html',
-  styleUrls: ['./student-account-activation.component.scss']
+  styleUrls: ['./student-account-activation.component.scss'],
 })
 export class StudentAccountActivationComponent implements OnInit {
-
   actionList = [
     {
-      action_id: "active",
-      action_text: "Active"
+      action_id: 'active',
+      action_text: 'Active',
     },
     {
-      action_id: "deactivate",
-      action_text: "Deactivate"
-    }
-  ]
+      action_id: 'deactivate',
+      action_text: 'Deactivate',
+    },
+  ];
   actionDropDownSetting: IDropdownSettings;
   studentListDropDownSetting: IDropdownSettings;
   studentListByStatus: Array<IStudent>;
@@ -30,43 +29,46 @@ export class StudentAccountActivationComponent implements OnInit {
   changeStudentStatusForm: UntypedFormGroup;
   updateStudentStatus: boolean;
 
-  constructor(private managementRestful: ForumBackendManagementService,
-              private formBuilder: UntypedFormBuilder) {
+  constructor(
+    private managementRestful: ForumBackendManagementService,
+    private formBuilder: UntypedFormBuilder
+  ) {
     this.actionDropDownSetting = {
       singleSelection: true,
       idField: 'action_id',
-      textField: 'action_text'
+      textField: 'action_text',
     };
     this.studentListDropDownSetting = {
       singleSelection: false,
-      idField: 'uuid',
+      idField: '_id',
       textField: 'itsc',
-      allowSearchFilter: true
-    }
+      allowSearchFilter: true,
+    };
     this.studentListByStatus = new Array<IStudent>();
     this.isLoading = false;
     this.changeStudentStatusForm = formBuilder.group({
-      studentList: ''
+      studentList: '',
     });
     this.updateStudentStatus = false;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onActionSelect(event: any) {
     this.isLoading = true;
     console.log(`onActionSelect: `, event);
-    this.updateStudentStatus = (event.action_id == "active");
-    this.managementRestful.getStudent(!(event.action_id == "active")).subscribe({
-      next: (studentList) => {
-        this.studentListByStatus = studentList;
-      },
-      complete: () => {
-        this.isLoading = false;
-        this.changeStudentStatusForm.get("studentList")?.setValue([]);
-      }
-    })
+    this.updateStudentStatus = event.action_id == 'active';
+    this.managementRestful
+      .getStudentStatus(!(event.action_id == 'active'))
+      .subscribe({
+        next: (studentList) => {
+          this.studentListByStatus = studentList;
+        },
+        complete: () => {
+          this.isLoading = false;
+          this.changeStudentStatusForm.get('studentList')?.setValue([]);
+        },
+      });
   }
 
   onActionDeSelect() {
@@ -75,14 +77,22 @@ export class StudentAccountActivationComponent implements OnInit {
 
   onSubmitChangeStudentStatusForm() {
     this.isLoading = true;
-    console.log(`onSubmitChangeStudentStatusForm: ${JSON.stringify(this.changeStudentStatusForm.value)}`);
-    const userIdList = this.changeStudentStatusForm.value.studentList.map((user: IStudent) => user.uuid);
+    console.log(
+      `onSubmitChangeStudentStatusForm: ${JSON.stringify(
+        this.changeStudentStatusForm.value
+      )}`
+    );
+    const userIdList = this.changeStudentStatusForm.value.studentList.map(
+      (user: IStudent) => user._id
+    );
     console.log(`userIdList: ${userIdList}`);
-    this.managementRestful.updateStudentStatus(userIdList, this.updateStudentStatus).subscribe({
-      complete: () => {
-        this.isLoading = false;
-        this.changeStudentStatusForm.get("studentList")?.setValue([]);
-      }
-    })
+    this.managementRestful
+      .updateStudentStatus(userIdList, this.updateStudentStatus)
+      .subscribe({
+        complete: () => {
+          this.isLoading = false;
+          this.changeStudentStatusForm.get('studentList')?.setValue([]);
+        },
+      });
   }
 }
--- src/app/layout/route-page/management-page/student-role/student-role.component.ts
+++ src/app/layout/route-page/management-page/student-role/student-role.component.ts
@@ -1,26 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {saveAs} from "file-saver";
-import {IStudentAccount} from "../../../../model/auth0-management/IStudentAccount";
-import {forkJoin, Observable} from "rxjs";
-import {UntypedFormBuilder, UntypedFormGroup} from "@angular/forms";
-import {IUserRole} from "../../../../model/auth0-management/IUserRole";
-import {IDropdownSettings} from "ng-multiselect-dropdown";
-import {ForumBackendManagementService} from "../../../../services/aws-lambda/forum-backend-management.service";
-import {ManagementDataService} from "../../../../services/data-services/management-data.service";
+import { Component, OnInit } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { IStudentAccount } from '../../../../model/auth0-management/IStudentAccount';
+import { forkJoin, Observable } from 'rxjs';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { IUserRole } from '../../../../model/auth0-management/IUserRole';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { ForumBackendManagementService } from '../../../../services/aws-lambda/forum-backend-management.service';
+import { ManagementDataService } from '../../../../services/data-services/management-data.service';
 
 @Component({
   selector: 'app-student-role',
   templateUrl: './student-role.component.html',
-  styleUrls: ['./student-role.component.scss']
+  styleUrls: ['./student-role.component.scss'],
 })
 export class StudentRoleComponent implements OnInit {
-
-  sampleCsv = [{
-    "email": 'abcdefg@connect.ust.hk',
-    "itsc": "abcdefg",
-    "sid": "123456",
-    "grad year": "2022"
-  }, {"email": "hijkl@connect.ust.hk", "itsc": "hijkl", "sid": "654321", "grad year": "2024"}];
+  sampleCsv = [
+    {
+      email: 'abcdefg@connect.ust.hk',
+      itsc: 'abcdefg',
+      sid: '123456',
+      'grad year': '2022',
+    },
+    {
+      email: 'hijkl@connect.ust.hk',
+      itsc: 'hijkl',
+      sid: '654321',
+      'grad year': '2024',
+    },
+  ];
 
   inputFilePlaceholder = '';
 
@@ -45,14 +52,19 @@ export class StudentRoleComponent implements OnInit {
   removeStudentAccountList: Array<IStudentAccount>;
   removeRoleForm: UntypedFormGroup;
 
-  constructor(private managementRestful: ForumBackendManagementService,
-              private formBuilder: UntypedFormBuilder,
-              private managementData: ManagementDataService) {
-    this.managementData$ = [this.managementRestful.getStudentAccountList(), this.managementRestful.getUserRolesList()];
+  constructor(
+    private managementRestful: ForumBackendManagementService,
+    private formBuilder: UntypedFormBuilder,
+    private managementData: ManagementDataService
+  ) {
+    this.managementData$ = [
+      this.managementRestful.getStudentAccountList(),
+      this.managementRestful.getUserRolesList(),
+    ];
 
     //delete student account
     this.deleteStudentForm = formBuilder.group({
-      users: ''
+      users: '',
     });
     this.isDeleteFormLoading = true;
 
@@ -64,18 +76,18 @@ export class StudentRoleComponent implements OnInit {
       idField: 'user_id',
       textField: 'username',
       allowSearchFilter: true,
-      enableCheckAll: true
+      enableCheckAll: true,
     };
     this.assignRoleDropDownSetting = {
       singleSelection: true,
       idField: 'id',
       textField: 'name',
       allowSearchFilter: true,
-      enableCheckAll: false
+      enableCheckAll: false,
     };
     this.assignRoleForm = formBuilder.group({
       role: '',
-      users: ''
+      users: '',
     });
     this.isAssignFormLoading = true;
 
@@ -85,16 +97,16 @@ export class StudentRoleComponent implements OnInit {
       idField: 'id',
       textField: 'name',
       allowSearchFilter: true,
-      enableCheckAll: false
+      enableCheckAll: false,
     };
     this.removeStudentAccountList = new Array<IStudentAccount>();
     this.removeRoleForm = formBuilder.group({
       role: '',
-      users: ''
+      users: '',
     });
     this.isRemoveFormLoading = true;
     this.isCreatingAccount = false;
-    this.inputFilePlaceholder = "Choose Student Account CSV";
+    this.inputFilePlaceholder = 'Choose Student Account CSV';
   }
 
   ngOnInit(): void {
@@ -102,22 +114,27 @@ export class StudentRoleComponent implements OnInit {
   }
 
   downloadSampleFile() {
-    const replacer = (key: string, value: string) => value === null ? '' : value // specify how you want to handle null values here
-    const header = Object.keys((this.sampleCsv)[0]);
+    const replacer = (key: string, value: string) =>
+      value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(this.sampleCsv[0]);
 
     const csv = [
       header.join(','), // header row first
-      // @ts-ignore
-      ...this.sampleCsv.map((row) => header.map((fieldName) => JSON.stringify(row[fieldName], replacer)).join(','))
+      ...this.sampleCsv.map((row) =>
+        header
+          // @ts-ignore
+          .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+          .join(',')
+      ),
     ].join('\r\n');
 
-    const blob = new Blob([csv], {type: 'text/csv'});
-    saveAs(blob, "SampleLoginInfo.csv");
+    const blob = new Blob([csv], { type: 'text/csv' });
+    saveAs(blob, 'SampleLoginInfo.csv');
   }
 
   uploadStudentCsv(event: any) {
     console.log(`event: `, event);
-    let csv: FileList = event.target.files
+    let csv: FileList = event.target.files;
     if (csv && csv.length > 0 && csv.item(0)) {
       let file: File | any = csv.item(0);
       console.log(`file: `, file);
@@ -128,25 +145,29 @@ export class StudentRoleComponent implements OnInit {
         let csv: string = reader.result as string;
         console.log(`uploaded csv: ${csv}`);
         this.managementData.studentAccountCsv = csv;
-      }
+      };
     }
   }
 
   createUser() {
-    console.log(`management data service: ${this.managementData.studentAccountCsv}`);
+    console.log(
+      `management data service: ${this.managementData.studentAccountCsv}`
+    );
     this.isCreatingAccount = true;
-    if (this.managementData.studentAccountCsv === "") {
+    if (this.managementData.studentAccountCsv === '') {
       alert('Empty CSV is uploaded. Please try again');
       this.reset();
     } else {
       //call auth0 management api
-      this.managementRestful.createLoginUser(this.managementData.studentAccountCsv).subscribe({
-        complete: () => {
-          alert('Student Account is created!');
-          this.reset();
-          this.fetchDataFromAuth0();
-        }
-      });
+      this.managementRestful
+        .createLoginUser({ student: this.managementData.studentAccountCsv })
+        .subscribe({
+          complete: () => {
+            alert('Student Account is created!');
+            this.reset();
+            this.fetchDataFromAuth0();
+          },
+        });
     }
   }
 
@@ -155,78 +176,96 @@ export class StudentRoleComponent implements OnInit {
     this.isRemoveFormLoading = true;
     this.managementRestful.getUserInRole(event.id).subscribe({
       next: (result) => {
-        this.removeStudentAccountList = result
+        this.removeStudentAccountList = result;
       },
       complete: () => {
         this.isRemoveFormLoading = false;
-      }
+      },
     });
   }
 
   reset() {
     this.isCreatingAccount = false;
-    this.managementData.studentAccountCsv = "";
-    this.inputFilePlaceholder = "Choose Student Account CSV";
+    this.managementData.studentAccountCsv = '';
+    this.inputFilePlaceholder = 'Choose Student Account CSV';
   }
 
   onSubmitDeleteStudentForm() {
-    console.log(`delete student form: ${JSON.stringify(this.deleteStudentForm.value)}`);
+    console.log(
+      `delete student form: ${JSON.stringify(this.deleteStudentForm.value)}`
+    );
     this.isDeleteFormLoading = true;
     if (!this.deleteStudentForm.value) {
       alert('Please choose student(s) account to delete');
       return;
     }
-    const studentAccountId = this.deleteStudentForm.value.users.map((student: any) => {
-      return {
-        auth0_id: student.user_id,
-        itsc: student.username
-      };
-    });
+    const studentAccountId = this.deleteStudentForm.value.users.map(
+      (student: any) => {
+        return {
+          auth0_id: student.user_id,
+          itsc: student.username,
+        };
+      }
+    );
     console.log(`studentAccountId: `, studentAccountId);
     this.managementRestful.deleteUserAccount(studentAccountId).subscribe({
       complete: () => {
         this.deleteStudentForm.reset();
         alert('Student(s) is remove from forum');
         this.fetchDataFromAuth0();
-      }
-    })
+      },
+    });
   }
 
   onSubmitAssignRoleForm() {
     this.isAssignFormLoading = true;
-    console.log(`assign role form: ${JSON.stringify(this.assignRoleForm.value)}`);
-    if (!this.assignRoleForm.value.role[0] || !this.assignRoleForm.value.users) {
+    console.log(
+      `assign role form: ${JSON.stringify(this.assignRoleForm.value)}`
+    );
+    if (
+      !this.assignRoleForm.value.role[0] ||
+      !this.assignRoleForm.value.users
+    ) {
       alert('Please choose role and student(s) to assign');
       return;
     }
     const roleId = this.assignRoleForm.value.role[0].id;
     console.log(`roleId: `, roleId);
-    const userIdList = this.assignRoleForm.value.users.map((user: IStudentAccount) => user.user_id);
+    const userIdList = this.assignRoleForm.value.users.map(
+      (user: IStudentAccount) => user.user_id
+    );
     console.log(`userList: `, userIdList);
     this.managementRestful.assignRoleToUsers(roleId, userIdList).subscribe({
       complete: () => {
         this.assignRoleForm.reset();
         alert('Role is assigned to user(s)');
         this.fetchDataFromAuth0();
-      }
+      },
     });
   }
 
   onSubmitRemoveRoleForm() {
-    console.log(`remove role form: ${JSON.stringify(this.removeRoleForm.value)}`);
+    console.log(
+      `remove role form: ${JSON.stringify(this.removeRoleForm.value)}`
+    );
     this.isRemoveFormLoading = true;
-    if (!this.removeRoleForm.value.role[0] || !this.removeRoleForm.value.users) {
+    if (
+      !this.removeRoleForm.value.role[0] ||
+      !this.removeRoleForm.value.users
+    ) {
       alert('Please choose role and student(s) to remove');
       return;
     }
     const roleId = this.removeRoleForm.value.role[0].id;
-    const userIdList = this.removeRoleForm.value.users.map((user: IStudentAccount) => user.user_id);
+    const userIdList = this.removeRoleForm.value.users.map(
+      (user: IStudentAccount) => user.user_id
+    );
     this.managementRestful.removeRoleFromUser(roleId, userIdList).subscribe({
       complete: () => {
         this.removeRoleForm.reset();
         alert('Role is remove from user(s)');
         this.fetchDataFromAuth0();
-      }
+      },
     });
   }
 
@@ -250,7 +289,7 @@ export class StudentRoleComponent implements OnInit {
         this.isAssignFormLoading = false;
         this.isRemoveFormLoading = false;
         this.isDeleteFormLoading = false;
-      }
+      },
     });
   }
 }
